b28d0d6fb0afd3a25e7397b3bf75d5a794e7e1bf
root <root@ADMINIS-3NJUFB7.>
2025-05-11 18:59:29 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Greetings from './components/Greetings';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Greetings name="Sabrin" surname="Aden" />
+      <Header />
+     
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,9 @@
+function Greetings(props) {
+    return (
+        <h1>
+            Hello, {props.name} {props.surname}!
+        </h1>
+    );
+}
+
+export default Greetings;
